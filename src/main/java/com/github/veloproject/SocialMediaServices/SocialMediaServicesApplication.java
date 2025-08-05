9596f3c6b8c141c94d73affb1e6cd78de5871e6c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-08-05 17:28:08 -0300

    feat: initial commit

---
+++ src/main/java/com/github/veloproject/SocialMediaServices/SocialMediaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.github.veloproject.SocialMediaServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaServicesApplication.class, args);
+	}
+
+}
