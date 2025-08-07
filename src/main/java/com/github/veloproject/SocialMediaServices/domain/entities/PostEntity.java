29f42b87d9c21c76180b36e0cbadda353cde68b9
Andrey Nardy <andreynardy@gmail.com>
2025-08-06 23:38:54 -0300

    feat: implementa entidade Post com autor e comunidade

--- src/main/java/com/github/veloproject/SocialMediaServices/domain/entities/PostEntity.java
+++ src/main/java/com/github/veloproject/SocialMediaServices/domain/entities/PostEntity.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.catalina.User;
+
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
@@ -17,4 +20,18 @@ public class PostEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_post")
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_community", nullable = false)
+    private CommunityEntity id_community;
+
+    public boolean community_associate;
+
+    @ManyToOne
+    @JoinColumn(name = "id_user")
+    private UserEntity user;
+
+    private String content;
+
+    private LocalDateTime  created_at;
 }
