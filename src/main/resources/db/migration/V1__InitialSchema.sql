defcfbbc253bcc0f2d4a42f9e6562824a8b08dea
jpaveiro <jpaveiros@gmail.com>
2025-08-09 22:42:43 -0300

    wip: modelando initial schema do flyway

--- src/main/resources/db/migration/V1__InitialSchema.sql
+++ src/main/resources/db/migration/V1__InitialSchema.sql
@@ -1,17 +1,6 @@
--- Tabela de usuários
-CREATE TABLE tb_users
-(
-    id_user                INT PRIMARY KEY AUTO_INCREMENT,
-    name                   VARCHAR(100) NOT NULL,
-    email                  VARCHAR(100) NOT NULL UNIQUE,
-    password               VARCHAR(255) NOT NULL,
-    community_id_community INT
-);
-
--- Tabela de comunidades
 CREATE TABLE tb_community
 (
-    id_community INT PRIMARY KEY AUTO_INCREMENT,
+    id_community SERIAL PRIMARY KEY,
     name         VARCHAR(100) NOT NULL,
     description  TEXT,
     created_by   INT          NOT NULL,
@@ -19,15 +8,13 @@ CREATE TABLE tb_community
     FOREIGN KEY (created_by) REFERENCES tb_users (id_user)
 );
 
--- Adicionando a chave estrangeira que estava faltando na tabela de usuários
 ALTER TABLE tb_users
     ADD CONSTRAINT fk_user_community
         FOREIGN KEY (community_id_community) REFERENCES tb_community (id_community);
 
--- Tabela de posts
 CREATE TABLE tb_posts
 (
-    id_post      INT PRIMARY KEY AUTO_INCREMENT,
+    id_post      SERIAL PRIMARY KEY,
     content      TEXT NOT NULL,
     community_id INT,
     user_id      INT  NOT NULL,
@@ -36,10 +23,9 @@ CREATE TABLE tb_posts
     FOREIGN KEY (user_id) REFERENCES tb_users (id_user)
 );
 
--- Tabela de comentários
 CREATE TABLE tb_comment
 (
-    id_comment INT PRIMARY KEY AUTO_INCREMENT,
+    id_comment SERIAL PRIMARY KEY,
     content    TEXT NOT NULL,
     post_id    INT  NOT NULL,
     user_id    INT  NOT NULL,
@@ -48,7 +34,6 @@ CREATE TABLE tb_comment
     FOREIGN KEY (user_id) REFERENCES tb_users (id_user)
 );
 
--- Tabela de hashtags
 CREATE TABLE tb_hashtag
 (
     id_hashtag  INT PRIMARY KEY AUTO_INCREMENT,
@@ -57,7 +42,6 @@ CREATE TABLE tb_hashtag
     created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Tabela de associação entre posts e hashtags
 CREATE TABLE tb_post_hashtag
 (
     post_id    INT NOT NULL,
@@ -67,7 +51,6 @@ CREATE TABLE tb_post_hashtag
     FOREIGN KEY (hashtag_id) REFERENCES tb_hashtag (id_hashtag)
 );
 
--- Tabela de likes em posts
 CREATE TABLE tb_like
 (
     user_id    INT NOT NULL,
