bb3f77b0622e693c192a6efff3557283de07c682
jpaveiro <jpaveiros@gmail.com>
2025-08-13 15:24:30 -0300

    refactor: acrescenta atributos às entities

--- domain/src/main/java/com/github/veloproject/socialmediaservices/domain/entities/HashtagEntity.java
+++ domain/src/main/java/com/github/veloproject/socialmediaservices/domain/entities/HashtagEntity.java
@@ -4,8 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Getter
 @Setter
 @Builder
 public class HashtagEntity {
+    private Integer id;
+    private Set<PostEntity> posts;
+    private Integer usageCount;
 }
--- domain/src/main/java/com/github/veloproject/socialmediaservices/domain/entities/UserEntity.java
+++ domain/src/main/java/com/github/veloproject/socialmediaservices/domain/entities/UserEntity.java
@@ -1,11 +1,22 @@
 package com.github.veloproject.socialmediaservices.domain.entities;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
-@Setter
+@Setter(AccessLevel.PRIVATE)
 @Builder
 public class UserEntity {
+    private Integer id;
+    private String name;
+    private String nickname;
+    private String bannerPhotoUrl;
+    private String profilePhotoUrl;
+    private Boolean isBlocked;
+    private Boolean isDeleted;
+    private LocalDateTime registeredAt;
 }
