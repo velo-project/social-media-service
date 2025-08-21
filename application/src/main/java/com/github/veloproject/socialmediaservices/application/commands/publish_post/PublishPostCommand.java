50c1f63b200f41c17b7532a456dbb9a612c7949d
jpaveiro <jpaveiros@gmail.com>
2025-08-21 16:53:37 -0300

    refactor: transforma command em record

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/publish_post/PublishPostCommand.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/publish_post/PublishPostCommand.java
@@ -1,6 +1,12 @@
 package com.github.veloproject.socialmediaservices.application.commands.publish_post;
 
 import com.github.veloproject.socialmediaservices.application.mediators.contracts.Request;
+import com.github.veloproject.socialmediaservices.domain.entities.CommunityEntity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 
-public class PublishPostCommand implements Request<PublishPostCommandResult> {
+public record PublishPostCommand(
+        @NotBlank @Max(355) String content,
+        CommunityEntity postedIn
+) implements Request<PublishPostCommandResult> {
 }
