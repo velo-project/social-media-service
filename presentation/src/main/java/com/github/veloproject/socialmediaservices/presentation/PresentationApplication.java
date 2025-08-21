ea5592347fab7f368183704ca06465fa80460652
jpaveiro <jpaveiros@gmail.com>
2025-08-21 03:08:56 -0300

    feat: busca por componentes do pacote com.github.veloproject.socialmediaservices

--- presentation/src/main/java/com/github/veloproject/socialmediaservices/presentation/PresentationApplication.java
+++ presentation/src/main/java/com/github/veloproject/socialmediaservices/presentation/PresentationApplication.java
@@ -2,8 +2,13 @@ package com.github.veloproject.socialmediaservices.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        // "com.github.veloproject",
+        "com.github.veloproject.socialmediaservices"
+})
 public class PresentationApplication {
 
     public static void main(String[] args) {
