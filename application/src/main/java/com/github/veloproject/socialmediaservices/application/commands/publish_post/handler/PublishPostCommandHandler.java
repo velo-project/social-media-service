a50399dbf6f382be1e9aa52c863a718c5f78a5c7
jpaveiro <jpaveiros@gmail.com>
2025-08-21 03:38:24 -0300

    feat: função handle no command handler de publish post

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/publish_post/handler/PublishPostCommandHandler.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/publish_post/handler/PublishPostCommandHandler.java
@@ -1,8 +1,15 @@
 package com.github.veloproject.socialmediaservices.application.commands.publish_post.handler;
 
+import com.github.veloproject.socialmediaservices.application.commands.publish_post.PublishPostCommand;
+import com.github.veloproject.socialmediaservices.application.commands.publish_post.PublishPostCommandResult;
+import com.github.veloproject.socialmediaservices.application.mediators.contracts.handlers.AuthRequestHandler;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PublishPostCommandHandler {
-
+public class PublishPostCommandHandler extends AuthRequestHandler<PublishPostCommand, PublishPostCommandResult> {
+    @Override
+    public PublishPostCommandResult handle(PublishPostCommand request, JwtAuthenticationToken token) {
+        return null;
+    }
 }
