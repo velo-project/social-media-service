81d3d5b2cf294081588c47cf8ee6ee6c13b2226f
jpaveiro <jpaveiros@gmail.com>
2025-08-21 17:14:11 -0300

    feat: community mapper funcional

--- infrastructure/src/main/java/com/github/veloproject/socialmediaservices/infrastructure/mappers/CommunityMapper.java
+++ infrastructure/src/main/java/com/github/veloproject/socialmediaservices/infrastructure/mappers/CommunityMapper.java
@@ -6,19 +6,27 @@ import com.github.veloproject.socialmediaservices.infrastructure.tables.Communit
 public class CommunityMapper {
     public static CommunitiesTable toPersistence(CommunityEntity e) {
         if (e == null) return null;
+        var createdBy = UserMapper.toPersistence(e.getCreatedBy());
 
         return new CommunitiesTable(
-                null,
-                null,
-                null,
-                null,
-                null
+                e.getId(),
+                e.getName(),
+                e.getDescription(),
+                createdBy,
+                e.getCreatedAt()
         );
     }
 
     public static CommunityEntity toDomain(CommunitiesTable t) {
         if (t == null) return null;
+        var createdBy = UserMapper.toDomain(t.getCreatedBy());
 
-        return null;
+        return CommunityEntity.builder()
+                .id(t.getId())
+                .name(t.getName())
+                .description(t.getDescription())
+                .createdBy(createdBy)
+                .createdAt(t.getCreatedAt())
+                .build();
     }
 }
