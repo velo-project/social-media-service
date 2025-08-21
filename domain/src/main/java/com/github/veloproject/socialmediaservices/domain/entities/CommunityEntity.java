0848930be7dd9845215da4181f1684d103d95970
jpaveiro <jpaveiros@gmail.com>
2025-08-21 17:13:57 -0300

    chore: atributos da entidade community

--- domain/src/main/java/com/github/veloproject/socialmediaservices/domain/entities/CommunityEntity.java
+++ domain/src/main/java/com/github/veloproject/socialmediaservices/domain/entities/CommunityEntity.java
@@ -1,11 +1,21 @@
 package com.github.veloproject.socialmediaservices.domain.entities;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
 public class CommunityEntity {
+    private Integer id;
+    private String name;
+    private String description;
+    @Setter(AccessLevel.PRIVATE)
+    private UserEntity createdBy;
+    @Setter(AccessLevel.PRIVATE)
+    private LocalDateTime createdAt;
 }
