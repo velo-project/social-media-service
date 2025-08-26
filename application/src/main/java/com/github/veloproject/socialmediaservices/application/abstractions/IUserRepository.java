160213685b20f6a0072de319c8097ab633179426
jpaveiro <jpaveiros@gmail.com>
2025-08-26 01:10:40 -0300

    refactor: adiciona @Repository ao invés de @Component ao IUserRepository

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IUserRepository.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IUserRepository.java
@@ -1,11 +1,11 @@
 package com.github.veloproject.socialmediaservices.application.abstractions;
 
 import com.github.veloproject.socialmediaservices.domain.entities.UserEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Component
+@Repository
 public interface IUserRepository {
     Optional<UserEntity> findById(Integer id);
 }
