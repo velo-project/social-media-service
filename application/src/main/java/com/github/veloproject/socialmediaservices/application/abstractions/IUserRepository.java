85ebadb60939d0bf3884ca1f4d78895cf9b57921
jpaveiro <jpaveiros@gmail.com>
2025-08-26 01:00:25 -0300

    fix: torna IUserRepository um componente

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IUserRepository.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IUserRepository.java
@@ -1,9 +1,11 @@
 package com.github.veloproject.socialmediaservices.application.abstractions;
 
 import com.github.veloproject.socialmediaservices.domain.entities.UserEntity;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public interface IUserRepository {
     Optional<UserEntity> findById(Integer id);
 }
