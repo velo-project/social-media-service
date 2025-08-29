13e69ed9b42830e69da991a3b9ab53d416b9abfe
jpaveiro <jpaveiros@gmail.com>
2025-08-29 01:50:52 -0300

    refactor: remove @Repository das abstrações de repositório

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IPostRepository.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IPostRepository.java
@@ -1,9 +1,7 @@
 package com.github.veloproject.socialmediaservices.application.abstractions;
 
 import com.github.veloproject.socialmediaservices.domain.entities.PostEntity;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface IPostRepository {
     Integer save(PostEntity user);
 }
--- application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IUserRepository.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IUserRepository.java
@@ -1,11 +1,9 @@
 package com.github.veloproject.socialmediaservices.application.abstractions;
 
 import com.github.veloproject.socialmediaservices.domain.entities.UserEntity;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface IUserRepository {
     Optional<UserEntity> findById(Integer id);
 }
