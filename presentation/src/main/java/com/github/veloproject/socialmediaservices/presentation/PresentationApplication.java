d79bf4a43162fcbead822ddeae5a59eb064aa5c5
jpaveiro <jpaveiros@gmail.com>
2025-08-29 15:28:31 -0300

    chore: scan de entidades e repositórios jpa na presentation

--- presentation/src/main/java/com/github/veloproject/socialmediaservices/presentation/PresentationApplication.java
+++ presentation/src/main/java/com/github/veloproject/socialmediaservices/presentation/PresentationApplication.java
@@ -2,13 +2,20 @@ package com.github.veloproject.socialmediaservices.presentation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {
-        // "com.github.veloproject",
         "com.github.veloproject.socialmediaservices"
 })
+@EntityScan(
+        basePackages = { "com.github.veloproject.socialmediaservices.infrastructure.tables" }
+)
+@EnableJpaRepositories(
+        basePackages = "com.github.veloproject.socialmediaservices.infrastructure.repositories.jpa"
+)
 public class PresentationApplication {
 
     public static void main(String[] args) {
