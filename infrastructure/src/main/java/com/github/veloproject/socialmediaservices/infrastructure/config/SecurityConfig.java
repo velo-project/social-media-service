45e3ab427950593a6a9427fdba54acd1fb1a4403
jpaveiro <jpaveiros@gmail.com>
2025-09-03 15:00:01 -0300

    chore: autoriza rota de publish apenas à usuários autenticados

--- infrastructure/src/main/java/com/github/veloproject/socialmediaservices/infrastructure/config/SecurityConfig.java
+++ infrastructure/src/main/java/com/github/veloproject/socialmediaservices/infrastructure/config/SecurityConfig.java
@@ -1,9 +1,5 @@
 package com.github.veloproject.socialmediaservices.infrastructure.config;
 
-import com.nimbusds.jose.jwk.JWK;
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,12 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 
 @Configuration
@@ -29,9 +22,6 @@ import java.security.interfaces.RSAPublicKey;
 public class SecurityConfig {
     @Value("${jwt.public.key}")
     private RSAPublicKey publicKey;
-
-    @Value("${jwt.private.key}")
-    private RSAPrivateKey privateKey;
 
     @Bean
     protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -43,11 +33,10 @@ public class SecurityConfig {
                         .permitAll()
 
                         .requestMatchers(HttpMethod.POST,
-                                "/api/social_media_services/post"
-                        )
-                        .permitAll()
+                                "/api/social_media/post/v1/publish"
+                        ).authenticated().anyRequest()
 
-                        .anyRequest().authenticated())
+                        .authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults()))
@@ -61,15 +50,5 @@ public class SecurityConfig {
         return NimbusJwtDecoder
                 .withPublicKey(this.publicKey)
                 .build();
-    }
-
-    @Bean
-    public JwtEncoder jwtEncoder() {
-        JWK jwk = new RSAKey
-                .Builder(this.publicKey)
-                .privateKey(this.privateKey)
-                .build();
-        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
-        return new NimbusJwtEncoder(jwks);
     }
 }
