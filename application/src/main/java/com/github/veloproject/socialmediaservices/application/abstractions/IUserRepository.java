a838f17ff3046e413e85d24d86c792879ef17bc7
jpaveiro <jpaveiros@gmail.com>
2025-09-06 22:54:29 -0300

    remove: descarta classes relacionadas à persistência da entidade usuário

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.veloproject.socialmediaservices.application.abstractions;
-
-import com.github.veloproject.socialmediaservices.domain.entities.UserEntity;
-
-import java.util.Optional;
-
-public interface IUserRepository {
-    Optional<UserEntity> findById(Integer id);
-}
