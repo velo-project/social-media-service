9c7e092225f7a4c1f71a83b50b6f3a583a0fb2fa
jpaveiro <jpaveiros@gmail.com>
2025-09-06 22:50:53 -0300

    fix: flyway agora reconhece pasta migration

--- presentation/src/main/resources/db/migration/V1__CreateInitialSchema.sql
+++ presentation/src/main/resources/db/migration/V1__CreateInitialSchema.sql
@@ -1,10 +1,20 @@
+CREATE TABLE tb_communities
+(
+    id_community SERIAL PRIMARY KEY,
+    name_community VARCHAR(50) NOT NULL,
+    description_community VARCHAR(255),
+    created_by INTEGER NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE
+);
+
 CREATE TABLE tb_posts
 (
     id_post SERIAL PRIMARY KEY,
     content_post TEXT NOT NULL,
-    posted_by REFERENCES tb_users(id_user),
-    id_community REFERENCES tb_communities(id_community),
-    posted_at TIMESTAMP WITHOUT TIME ZONE
+    posted_by INTEGER NOT NULL,
+    id_community INTEGER,
+    posted_at TIMESTAMP WITHOUT TIME ZONE,
+    FOREIGN KEY (id_community) REFERENCES tb_communities(id_community)
 );
 
 CREATE TABLE tb_hashtags
@@ -21,13 +31,4 @@ CREATE TABLE tb_post_hashtags
     PRIMARY KEY(id_post, id_hashtag),
     FOREIGN KEY (id_post) REFERENCES tb_posts(id_post) ON DELETE CASCADE,
     FOREIGN KEY (id_hashtag) REFERENCES tb_hashtags(id_hashtag) ON DELETE CASCADE
-);
-
-CREATE TABLE tb_communities
-(
-    id_community SERIAL PRIMARY KEY,
-    name_community VARCHAR(50) NOT NULL,
-    description_community VARCHAR(255),
-    created_by REFERENCES tb_users(id_user),
-    created_at TIMESTAMP WITHOUT TIME ZONE
 );
