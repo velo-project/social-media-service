c1b4cf011a585bd6102e85a5b5f417707a7a954f
jpaveiro <jpaveiros@gmail.com>
2025-09-17 02:59:29 -0300

    feat(postRepository): métodos deleteById, findById, existsById, findAllByUserId

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IPostRepository.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/abstractions/IPostRepository.java
@@ -2,6 +2,13 @@ package com.github.veloproject.socialmediaservices.application.abstractions;
 
 import com.github.veloproject.socialmediaservices.domain.entities.PostEntity;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface IPostRepository {
     Integer save(PostEntity user);
+    void deleteById(Integer postId);
+    Optional<PostEntity> findById(Integer id);
+    boolean existsById(Integer postId);
+    List<PostEntity> findAllByUserId(Integer userId);
 }
