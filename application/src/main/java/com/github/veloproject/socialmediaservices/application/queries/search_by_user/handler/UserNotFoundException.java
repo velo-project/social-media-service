3f17690e20f2ef147fb798f3d40847e85c353535
jpaveiro <jpaveiros@gmail.com>
2025-09-17 03:02:34 -0300

    remove: search by user query

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/queries/search_by_user/handler/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.veloproject.socialmediaservices.application.queries.search_by_user.handler;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
