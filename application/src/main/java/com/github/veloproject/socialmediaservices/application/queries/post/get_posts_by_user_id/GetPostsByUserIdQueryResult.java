00196151336e24b48ba5eb7e49f07dc8e39a3eab
jpaveiro <jpaveiros@gmail.com>
2025-09-22 02:27:14 -0300

    feat: get posts by user id query

---
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/queries/post/get_posts_by_user_id/GetPostsByUserIdQueryResult.java
@@ -0,0 +1,16 @@
+package com.github.veloproject.socialmediaservices.application.queries.post.get_posts_by_user_id;
+
+import com.github.veloproject.socialmediaservices.application.mediators.contracts.Response;
+import com.github.veloproject.socialmediaservices.domain.entities.PostEntity;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+@Getter
+public class GetPostsByUserIdQueryResult extends Response {
+    private final Page<PostEntity> posts;
+
+    public GetPostsByUserIdQueryResult(Integer statusCode, Page<PostEntity> posts) {
+        super(statusCode);
+        this.posts = posts;
+    }
+}
---
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/queries/post/get_posts_by_user_id/handler/GetPostsByUserIdQueryHandler.java
@@ -0,0 +1,27 @@
+package com.github.veloproject.socialmediaservices.application.queries.post.get_posts_by_user_id.handler;
+
+import com.github.veloproject.socialmediaservices.application.abstractions.IPostRepository;
+import com.github.veloproject.socialmediaservices.application.mediators.contracts.handlers.NoAuthRequestHandler;
+import com.github.veloproject.socialmediaservices.application.queries.post.get_posts_by_user_id.GetPostsByUserIdQuery;
+import com.github.veloproject.socialmediaservices.application.queries.post.get_posts_by_user_id.GetPostsByUserIdQueryResult;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GetPostsByUserIdQueryHandler extends NoAuthRequestHandler<GetPostsByUserIdQuery, GetPostsByUserIdQueryResult> {
+    private final IPostRepository postRepository;
+
+    public GetPostsByUserIdQueryHandler(IPostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public GetPostsByUserIdQueryResult handle(GetPostsByUserIdQuery request) {
+        var pageRequest = PageRequest.of(0, request.page(), Sort.by("postedIn").descending());
+        var posts = postRepository.findPageByUserId(request.userId(), pageRequest);
+
+        return new GetPostsByUserIdQueryResult(200, posts);
+    }
+
+}
