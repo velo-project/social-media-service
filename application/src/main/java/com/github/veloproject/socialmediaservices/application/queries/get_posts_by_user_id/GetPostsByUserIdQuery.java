e5ad051b40d256121e5188bbef888687fb5ef7b2
jpaveiro <jpaveiros@gmail.com>
2025-09-22 02:25:20 -0300

    refactor: mudança de diretórios para melhor organização

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/queries/get_posts_by_user_id/GetPostsByUserIdQuery.java
+++
@@ -1,6 +0,0 @@
-package com.github.veloproject.socialmediaservices.application.queries.get_posts_by_user_id;
-
-import com.github.veloproject.socialmediaservices.application.mediators.contracts.Request;
-
-public record GetPostsByUserIdQuery(Integer userId, Integer page) implements Request<GetPostsByUserIdQueryResult> {
-}
