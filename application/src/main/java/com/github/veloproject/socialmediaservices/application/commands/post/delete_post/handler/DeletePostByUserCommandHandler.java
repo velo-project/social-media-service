66258c4108574b01165df2c04938d1af22137939
jpaveiro <jpaveiros@gmail.com>
2025-09-24 02:50:42 -0300

    feat: verificação se o usuário do jwt é válido

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/post/delete_post/handler/DeletePostByUserCommandHandler.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/post/delete_post/handler/DeletePostByUserCommandHandler.java
@@ -1,10 +1,12 @@
 package com.github.veloproject.socialmediaservices.application.commands.post.delete_post.handler;
 
 import com.github.veloproject.socialmediaservices.application.abstractions.IPostRepository;
+import com.github.veloproject.socialmediaservices.application.abstractions.IUserServices;
 import com.github.veloproject.socialmediaservices.application.commands.post.delete_post.DeletePostByUserCommand;
 import com.github.veloproject.socialmediaservices.application.commands.post.delete_post.DeletePostByUserCommandResult;
 import com.github.veloproject.socialmediaservices.application.mediators.contracts.handlers.AuthRequestHandler;
 import com.github.veloproject.socialmediaservices.domain.exceptions.InvalidPostProvidedException;
+import com.github.veloproject.socialmediaservices.domain.exceptions.InvalidUserProvidedException;
 import com.github.veloproject.socialmediaservices.domain.exceptions.UserNotAuthorException;
 import jakarta.transaction.Transactional;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -13,9 +15,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class DeletePostByUserCommandHandler extends AuthRequestHandler<DeletePostByUserCommand, DeletePostByUserCommandResult> {
     private final IPostRepository postRepository;
+    private final IUserServices userServices;
 
-    public DeletePostByUserCommandHandler(IPostRepository postRepository) {
+    public DeletePostByUserCommandHandler(IPostRepository postRepository,
+                                          IUserServices userServices) {
         this.postRepository = postRepository;
+        this.userServices = userServices;
     }
 
     @Transactional
@@ -26,6 +31,9 @@ public class DeletePostByUserCommandHandler extends AuthRequestHandler<DeletePos
                 .findById(request.postId()).orElseThrow(InvalidPostProvidedException::new);
 
         var userId = Integer.valueOf(token.getToken().getSubject());
+        if (!userServices.existsById(userId))
+            throw new InvalidUserProvidedException();
+
         if (!post.getPostedBy().equals(userId))
             throw new UserNotAuthorException();
 
