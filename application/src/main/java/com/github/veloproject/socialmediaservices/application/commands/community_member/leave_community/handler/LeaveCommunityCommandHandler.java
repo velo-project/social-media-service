a4bb04379f80cac6e46381c9189bc5f098c0d322
jpaveiro <jpaveiros@gmail.com>
2025-09-28 00:54:18 -0300

    feat: dono da comunidade não pode sair da mesma

--- application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/community_member/leave_community/handler/LeaveCommunityCommandHandler.java
+++ application/src/main/java/com/github/veloproject/socialmediaservices/application/commands/community_member/leave_community/handler/LeaveCommunityCommandHandler.java
@@ -6,10 +6,8 @@ import com.github.veloproject.socialmediaservices.application.abstractions.IUser
 import com.github.veloproject.socialmediaservices.application.commands.community_member.leave_community.LeaveCommunityCommand;
 import com.github.veloproject.socialmediaservices.application.commands.community_member.leave_community.LeaveCommunityCommandResult;
 import com.github.veloproject.socialmediaservices.application.mediators.contracts.handlers.AuthRequestHandler;
-import com.github.veloproject.socialmediaservices.domain.exceptions.InvalidCommunityProvidedException;
-import com.github.veloproject.socialmediaservices.domain.exceptions.InvalidUserProvidedException;
-import com.github.veloproject.socialmediaservices.domain.exceptions.UserAlreadyInCommunityException;
-import com.github.veloproject.socialmediaservices.domain.exceptions.UserNotInCommunityException;
+import com.github.veloproject.socialmediaservices.domain.exceptions.*;
+import jakarta.transaction.Transactional;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +23,27 @@ public class LeaveCommunityCommandHandler extends AuthRequestHandler<LeaveCommun
         this.userServices = userServices;
     }
 
+    @Transactional
     @Override
     public LeaveCommunityCommandResult handle(LeaveCommunityCommand request,
                                               JwtAuthenticationToken token) {
         var integerSubject = Integer.valueOf(token.getToken().getSubject());
+        var community = communityRepository.findById(request.communityId());
+
         if (!userServices
                 .existsById(integerSubject)) throw new InvalidUserProvidedException();
-        else if (!communityRepository
-                .existsById(request.communityId())) throw new InvalidCommunityProvidedException();
+
+        else if (community
+                .isEmpty()) throw new InvalidCommunityProvidedException();
+
+        else if (community.get()
+                .getCreatedBy()
+                .equals(integerSubject)) throw new UserIsTheCommunityOwnerException();
+
         else if (!communityMemberRepository
                 .existsMember(integerSubject, request.communityId())) throw new UserNotInCommunityException();
 
         communityMemberRepository.delete(request.communityId(), integerSubject);
-        return new  LeaveCommunityCommandResult(200);
+        return new LeaveCommunityCommandResult(200);
     }
 }
