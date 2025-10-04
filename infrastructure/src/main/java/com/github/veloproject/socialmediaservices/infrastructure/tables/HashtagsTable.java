de8cd810b1581facafb04577460911a2187d99ec
jpaveiro <jpaveiros@gmail.com>
2025-10-04 17:42:15 -0300

    refactor: remove dependências não utilizadas

--- infrastructure/src/main/java/com/github/veloproject/socialmediaservices/infrastructure/tables/HashtagsTable.java
+++ infrastructure/src/main/java/com/github/veloproject/socialmediaservices/infrastructure/tables/HashtagsTable.java
@@ -1,12 +1,9 @@
 package com.github.veloproject.socialmediaservices.infrastructure.tables;
 
-import com.github.veloproject.socialmediaservices.domain.entities.HashtagEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.Set;
 
 @NoArgsConstructor
 @Getter
