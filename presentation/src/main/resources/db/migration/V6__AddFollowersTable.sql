1840c2b7335249ce64b570d34a504d2161d6941b
jpaveiro <jpaveiros@gmail.com>
2025-10-07 22:44:32 -0300

    fix: arruma nome do atributo

--- presentation/src/main/resources/db/migration/V6__AddFollowersTable.sql
+++ presentation/src/main/resources/db/migration/V6__AddFollowersTable.sql
@@ -1,5 +1,6 @@
 CREATE TABLE tb_user_followers(
     id_user INT NOT NULL,
-    id_following INT NOT NULL,
-    PRIMARY KEY (id_user, id_following)
+    id_follower INT NOT NULL,
+    followed_at TIMESTAMP WITHOUT TIME ZONE,
+    PRIMARY KEY (id_user, id_follower)
 );
